c5404a00e8a36fc5e9266c97649835c653be7ee6
PerceptronV <neutrinovs@gmail.com>
2023-04-09 07:40:19 +0800

    Non-recursive Tarjan

--- experiments/scc.jl
+++ experiments/scc.jl
@@ -20,53 +20,84 @@ include("common.jl")
 
 
 function tarjan(wg::WikigraphUnweighed)
-    trueIDs = [i for i = 1:wg.pm.totalpages if notRedir(wg.pm, i)]
-
-    stack = Stack{Int32}()
     t = 0
+    stack = Stack{Int32}()
 
-    times = Dict{Int32, Int32}()
+    times = Int32[0 for _ = 1:wg.pm.totalpages]
+    lowLinks = Int32[0 for _ = 1:wg.pm.totalpages]
+    parents = Int32[0 for _ = 1:wg.pm.totalpages]
+    positions = Int32[0 for _ = 1:wg.pm.totalpages]
+    lens = [length(i) for i in wg.links]
+
     onStack = Set{Int32}()
-    searched = Set{Int32}()
     scc = Set{Int32}[]
 
-    function strongConnect(u)
+    function init(u)
         t += 1
         times[u] = t
-        lowlink = t
+        lowLinks[u] = t
         push!(stack, u)
         push!(onStack, u)
-        push!(searched, u)
-
-        for v in wg.links[u]
-            if v ∉ searched
-                vlow = strongConnect(v)
-                lowlink = min(lowlink, vlow)
-            elseif v in onStack
-                lowlink = min(lowlink, times[v])
-            end
-        end
-
-        if lowlink == times[u]
-            component = Set{Int32}()
-            while (v = pop!(stack)) != u
-                pop!(onStack, v)
-                push!(component, v)
-            end
-            pop!(onStack, u)
-            push!(component, u)
-            push!(scc, component)
-        end
-
-        return lowlink
     end
 
+    function createScc(u)
+        component = Set{Int32}()
+        while (v = pop!(stack)) != u
+            pop!(onStack, v)
+            push!(component, v)
+        end
+        pop!(onStack, u)
+        push!(component, u)
+        push!(scc, component)
+    end
+
+    function next(u, src)
+        pos = positions[u] + 1
+        
+        for i = pos:lens[u]
+            v = wg.links[u][i]
+
+            if times[v] == 0
+                positions[u] = i
+                parents[v] = u
+                return v, true
+            end
+
+            (v in onStack) && (lowLinks[u] = min(lowLinks[u], times[v]))
+        end
+
+        if lowLinks[u] == times[u]
+            createScc(u)
+        end
+
+        if u == src
+            return 0, false
+        end
+        
+        return parents[u], false
+    end
+
+    function strongConnect(src)
+        init(src)
+        u = src
+
+        while ((v, new) = next(u, src)) != (0, false)
+            if new
+                init(v)
+            else
+                lowLinks[v] = min(lowLinks[v], lowLinks[u])
+            end
+            u = v
+        end
+    end
+
+    trueIDs = [i for i = 1:wg.pm.totalpages if notRedir(wg.pm, i)]
+
     for u in ProgressBar(trueIDs)
-        (u in searched) || strongConnect(u)
+        (times[u] == 0) && strongConnect(u)
     end
 
     return scc
 end
-
 
 scc = tarjan(fwg)
