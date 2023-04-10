139793ff32dd212aa2a1dc819c0b984e741464a8
PerceptronV <neutrinovs@gmail.com>
2023-04-10 17:00:26 +0800

    Undo normalsiation fix

--- utils.jl
+++ utils.jl
@@ -22,16 +22,7 @@ using JLD2, ProgressBars
 # Utility functions
 
 function normalise(title::String)
-    if length(title) == 0
-        return title
-    end
-
-    u = Int(codepoint(title[begin]))
-    if 97 <= u <= 122
-        return replace(uppercasefirst(strip(title)), " " => "_")
-    end
-    
-    return replace(strip(title), " " => "_")
+    return replace(uppercasefirst(strip(title)), " " => "_")
 end
 
 checkfile(f) = ispath(f) && rm(f)
